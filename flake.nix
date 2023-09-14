f5497b104dd886dfc4f3d06f0c5179cbd3b84253
Michon van Dooren <me@maienm.com>
2023-09-14 17:03:24 +0200

    Init project

--- flake.nix
+++ flake.nix
@@ -1,20 +1,33 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
 
     flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          cargo
-        ];
-      };
-    }
-  );
+  outputs = { nixpkgs, fenix, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        devToolchain = fenix.packages.${system}.stable;
+        devToolchainUnstable = fenix.packages."${system}".latest;
+      in
+      {
+        defaultPackage = fenix.packages.x86_64-linux.minimal.toolchain;
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            (devToolchain.withComponents [
+              "cargo"
+              "rust-src"
+              "rustc"
+            ])
+            (devToolchainUnstable.withComponents [
+              "rustfmt"
+            ])
+            fenix.packages.${system}.rust-analyzer
+          ];
+        };
+      });
 }
